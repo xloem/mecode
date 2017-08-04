f4a3f0c6c03d479021301cff1a771dd1a82de1fb
RobWks <robertweeks94@gmail.com>
2017-08-04 11:22:12 -0400

    Revert "Test of new scanning code."
    This reverts commit e7f65480a4b058d37d364a249b30ee60d98b4d04.

--- mecode/main.py
+++ mecode/main.py
@@ -790,17 +790,11 @@ class G(object):
         ----------
         path : str
             The path specifying the aerotech calibration file.
+
         """
         self.write(r'LOADCALFILE "{}", 2D_CAL'.format(path))
 
     def toggle_pressure(self, com_port):
-        """ Toggle pressure on/off of Nod
-
-        Parameters
-        ----------
-        path : str
-            The path specifying the aerotech calibration file.
-        """
         self.write('Call togglePress P{}'.format(com_port))
 
     def set_pressure(self, com_port, value):
@@ -812,7 +806,26 @@ class G(object):
     def set_valve(self, num, value):
         self.write('$DO{}.0={}'.format(num, value))
 
-    def scanArea(self,x_start,x_stop, y_start, y_stop, y_step=self.IXPitch):
+    def genCRC8(self,data):
+        CRC8 = 0
+        for letter in list(bytearray(data)):
+            for i in range(8):
+                if (letter^CRC8)&0x01:
+                    CRC8 ^= 0x18
+                    CRC8 >>= 1
+                    CRC8 |= 0x80
+                else:
+                    CRC8 >>= 1
+                letter >>= 1
+        return data +'{:02X}'.format(CRC8)
+
+    def omni_on(self, com_port):
+        self.write('Call omniOn P{}'.format(com_port))
+
+    def omni_off(self, com_port):
+        self.write('Call omniOff P{}'.format(com_port))
+
+    def scanArea(self,x_start,x_stop, y_start, y_stop, y_step=0.3):
         start_string="""PSOCONTROL Y RESET
 PSOTRACK Y INPUT 7
 PSODISTANCE Y FIXED ABS(UNITSTOCOUNTS(Y, {}/16))
@@ -821,18 +834,20 @@ PSOOUTPUT Y PULSE
 PSOCONTROL Y ARM""".format(y_step)
         stop_string = "PSOCONTROL Y OFF"
 
-        #Scanner constants
-        scan_width = self.scan.scan_width
+        #Scnnaer constants
+        scan_buffer = 15.0
+        scan_width = 150.0
 
         #Actual scanning area in x
-        area_width = x_stop - x_start
+        area_x_start = x_start - scan_buffer
+        area_x_stop = x_stop + scan_buffer
+        area_width = area_x_stop - area_x_start
         num_passes = int(math.ceil(area_width/scan_width))
-        scan_trim = (scan_width*num_passes-area_width)/2
 
         if num_passes % 2 == 0: #Is even
-            scan_x_start = x_start + area_width/2 - (scan_width/2+(num_passes/2-1)*scan_width)
+            scan_x_start = area_x_start + area_width/2 - (scan_width/2*num_passes/2)
         else: #Is odd
-            scan_x_start = x_start + area_width/2 - (scan_width*(num_passes-1)/2)
+            scan_x_start = area_x_start + area_width/2 - (scan_width*(num_passes-1)/2)
         scan_x_length = (num_passes-1)*scan_width
 
         #Actual scanning area in y
@@ -849,7 +864,7 @@ PSOCONTROL Y ARM""".format(y_step)
         from multiprocessing.pool import ThreadPool
         pool = ThreadPool(processes=1)
         result = pool.apply_async(self.scan.continuous_get_profiles,(profile_count,))
-        
+
         #Start scan
         self.write(start_string)
         self.relative()
@@ -858,8 +873,8 @@ PSOCONTROL Y ARM""".format(y_step)
         else:
             self.move(y=scan_y_length)
         self.write(stop_string)
-        
-        return processScanResults(result.get(),num_passes,scan_width,scan_trim), x_start, y_start, y_step
+    
+        return result.get()
 
     # Public Interface  #######################################################
 
