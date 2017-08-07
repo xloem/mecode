77aa91966a31315a92307f64ca3c671f13eb9dc5
RobWks <robertweeks94@gmail.com>
2017-08-07 11:32:27 -0400

    First semi-working version of the calibration script.

--- mecode/devices/Keyence/keyence_calibration.py
+++ mecode/devices/Keyence/keyence_calibration.py
@@ -4,9 +4,14 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import cv2
 
-scan = np.load('calibration_fixture.npy')
+scan = np.load('calibration_verification.npy')
 gradX, gradY = np.nan_to_num(np.gradient(scan)) #[0]:X [1]:Y
 sides = [np.copy(gradX),np.copy(gradX),np.copy(gradY),np.copy(gradY)]
+
+#Key parameters
+fit = 0.03
+canny = [250,375]
+angle = [0.3,0.5]
 
 def fit_to_plane(points):
 	xs = points[:,0]
@@ -65,55 +70,62 @@ planes = []
 for index, side in enumerate(sides):
 	for x in np.nditer(side,op_flags=['readwrite']):
 		if index%2 == 0:
-			if x < -0.6 or x > -0.2:
+			if x < -angle[1] or x > -angle[0]:
 				x[...] = 0
 		else:
-			if x > 0.6 or x < 0.2:
+			if x > angle[1] or x < angle[0]:
 				x[...] = 0
 	if index%2 == 0:
 		side = side/np.amin(side)
 	else:
 		side = side/np.amax(side)
-	#side = Image.fromarray(np.uint8(side*255))
-	#side.save('side{}.png'.format(index+1))
-	#side = cv2.imread('side{}.png'.format(index+1))
 	
 	side = np.uint8(side*255)
 	
 	if index in [1,2,3]:
-		gray = cv2.bilateralFilter(side,5,750,750)
-		edged = cv2.Canny(gray, 250, 500)
-		#cv2.imshow('gray{}'.format(index+1),gray)
-		#cv2.imshow('edged{}'.format(index+1),edged)
-		kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-		closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-		_, cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	#if index in [1]:
+		gray = cv2.GaussianBlur(side,(9,9),0)
+		#gray = cv2.bilateralFilter(blur,7,650,1750)
+		edged = cv2.Canny(gray, canny[0], canny[1])
+		#cv2.imshow('gray{}'.format(index),gray)
+		#cv2.imshow('edged{}'.format(index),edged)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
+
+		#kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
+		#closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+		#_, cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+		dilated = cv2.dilate(edged, kernel)
+		#cv2.imshow('dilated{}'.format(index),dilated)
+		_, cnts, _ = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 		largeArea = 0
 		# loop over the contours
 		for c in cnts:
 			# approximate the contour
 			peri = cv2.arcLength(c, True)
-			approx = cv2.approxPolyDP(c, 0.03 * peri, True)
+			approx = cv2.approxPolyDP(c, fit * peri, True)
 			#cv2.drawContours(side, [approx], -1, (255), 2)
-
-			if len(approx) == 4:
-				if cv2.contourArea(approx) > largeArea:
-					largestContour = approx
-					largeArea = cv2.contourArea(approx)
+			if cv2.contourArea(approx) > largeArea:
+				#if len(approx) == 4:
+				largestContour = approx
+				largeArea = cv2.contourArea(approx)
 		cv2.drawContours(side, [largestContour], -1, (255), 3)
-		#print(largestContour)
+		print(len(largestContour))
 		#cv2.imshow('img{}'.format(index+1),side)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
 
 		polygon = list(largestContour.flatten().astype(int))
-		img = Image.new('1',(len(scan),len(scan[0])),0)
+		img = Image.new('1',(len(scan[0]),len(scan)),0)
 		ImageDraw.Draw(img).polygon(polygon,outline=1,fill=1)
 		mask = np.array(img)
-		#img.show()
-
-		IXStart = -120.0
+		img.show()
+		
+		IXStart = -0.1
 		IXPitch = 0.3
-		IYStart = 0
+		IYStart = 0.3
 		IYPitch = 0.3
 		points = []
 		for i in range(len(scan)):	
@@ -124,10 +136,6 @@ for index, side in enumerate(sides):
 					z = scan[i,j]
 					points.append([x,y,z])
 		planes.append(fit_to_plane(np.array(points)))
-
+		
 print("The location of the pyramid is:")
 print("x:{:.2f} y:{:.2f} z:{:.2f}".format(*intersection_of_planes(planes)))
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-
