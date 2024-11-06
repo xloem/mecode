d9b20ac865665ea6f544ce909f0c46918ff55c39
Rodrigo Telles <rtellez700@gmail.com>
2024-11-06 13:40:08 -0800

    v0.4.6

--- mecode/__init__.py
+++ mecode/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Rodrigo Telles"""
 __email__ = 'rtelles@g.harvard.edu'
-__version__ = '0.4.5'
+__version__ = '0.4.6'
 
 from mecode.main import G, is_str, decode2To3
 from mecode.matrix import GMatrix
