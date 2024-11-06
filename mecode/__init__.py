39247f43bb6603f941a4cc31ded7a5cdd09e4cab
Rodrigo Telles <rtellez700@gmail.com>
2024-11-06 13:33:15 -0800

    initial hatch configuration

--- mecode/__init__.py
+++ mecode/__init__.py
@@ -1,3 +1,9 @@
+"""Top-level package for mecode."""
+
+__author__ = """Rodrigo Telles"""
+__email__ = 'rtelles@g.harvard.edu'
+__version__ = '0.4.4'
+
 from mecode.main import G, is_str, decode2To3
 from mecode.matrix import GMatrix
 from mecode.matrix3D import GMatrix3D
